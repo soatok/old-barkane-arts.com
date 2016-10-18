c699d1e28305f75e0d10abc4092103feb5c30050
Soatok <soatok@yahoo.com>
2016-10-18 01:04:56 -0400

    Create public_html/index.php
    Adding nginx configuration (yes, we're using LetsEncrypt)
    
    Add Halite license exemption from @paragonie-security
    
    Add composer.json, .gitignore
    
    add nikic/fastroute to composer.json
    
    Added the comma I forgot in the last edit.
    
    Something silly

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # barkane-arts.com
+Congratulations, you are one of the few people who actually bothers to follow instructions when told READ ME.
---
+++ public_html/index.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+require_once \dirname(__DIR__) . '/vendor/autoload.php';
+
+/**
+ * Hi everyone!
+ *
+ * This is a starter project for Soatok Dreamseeker and Rudolf Whitechest.
+ */
+
+echo 'Hello, world! :3', PHP_EOL;
