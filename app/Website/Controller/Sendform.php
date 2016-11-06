710797e232f6ee6a2890508bea3be2e634d09434
Soatok <soatok@yahoo.com>
2016-11-06 04:36:50 -0500

    Changes made:
    * We put all of our code in a class called 'Sendform',
      in a method called 'contact', since that's what the
      routes.tpl.php defines.
    * Move the homedir to the DATA_DIR.
    * Use the framework's `$this->getPostData()` method.
      This only returns data if a CSRF attack isn't being performed.
    * Added `$this->view()` which loads the template.
    * Instead of echoing this error messages directly, let's pass it to the
      template to handle displaying them. The Controller doesn't
      care how data is presented; that's the View's job. The Controller
      also doesn't care how data is stored or organized; the Model
      handles that. The Controller's job is to be invoked on page load,
      run some code that may interact with any number of Models, then
      pass the result to a given View. This is the basics of an MVC
      architecture.

--- app/Website/Controller/Sendform.php
+++ app/Website/Controller/Sendform.php
@@ -2,42 +2,42 @@
 declare(strict_types=1);
 namespace BarkaneArts\Website\Controller;
 
-use \ParagonIE\GPGMailer\GPGMailer;
-use \Zend\Mail\Message;
-use \Zend\Mail\Transport\Sendmail;
+use BarkaneArts\Framework\Controller;
+use ParagonIE\GPGMailer\GPGMailer;
+use Zend\Mail\Message;
+use Zend\Mail\Transport\Sendmail;
+use Zend\Validator\EmailAddress;
 
-$validator= new Zend\Validator\EmailAddress();
-
-if($validator->isValid($_POST['email']))
+class Sendform extends Controller
 {
-    $message = new Message;
-    $message->setBodyText(strip_tags($_POST['message']));
-    $message->setBodyHtml($_POST['message']);
-    $message->setFrom($_POST['email'],$_POST['name']);
-    $message->addTo('djerale@gmail.com','David Jerale');
-    $message->setSubject($_POST['subject'] . " -Barkane Arts");
-
-
-    $gpgMailer = new GPGMailer(
-        new Sendmail(),
-        ['homedir' => '/homedir/containing/keyring']
-    );
-    $fingerprint = '052EC4EF72EF66F3BBE3C74385A4C89D9BDFA52D';
-    $gpgMailer->send($message, $fingerprint);
-
-    echo "Message sent";
-}
-else
-{
-// invalid email
-    foreach($validator->getMessages() as $errorMessage)
+    public function contact()
     {
-        echo "$errorMessage<br/>";
+        $validator= new EmailAddress();
+
+        if ($post = $this->getPostData()) {
+            if ($validator->isValid($post['email'])) {
+                $message = new Message;
+                $message->setBody($post['message']);
+                $message->setFrom($post['email'], $post['name']);
+                $message->addTo('djerale@gmail.com', 'David Jerale');
+                $message->setSubject($post['subject'] . " -Barkane Arts");
+
+
+                $gpgMailer = new GPGMailer(
+                    new Sendmail(),
+                    ['homedir' => DATA_DIR]
+                );
+                $fingerprint = '052EC4EF72EF66F3BBE3C74385A4C89D9BDFA52D';
+                $gpgMailer->send($message, $fingerprint);
+
+                echo "Message sent";
+            } else {
+                $this->twig->addGlobal(
+                    'error_messages',
+                    $validator->getMessages()
+                );
+            }
+        }
+        $this->view('contact/contact');
     }
 }
-/**
- * Created by PhpStorm.
- * User: david
- * Date: 11/2/16
- * Time: 9:28 PM
- */
